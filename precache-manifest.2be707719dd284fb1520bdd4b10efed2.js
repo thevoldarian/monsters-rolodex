3989837e22eef974d7b9e7f1d07e108e524d9355
Beauchamp <beaucham@amazon.com>
2020-05-01 15:46:18 -0400

    Updates

---
+++ precache-manifest.2be707719dd284fb1520bdd4b10efed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01dedd7519a72edcf3b21046908f6601",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5697b2cb5cf4dc56e81",
+    "url": "/monsters-rolodex/static/css/main.fa110d50.chunk.css"
+  },
+  {
+    "revision": "e4cfa178a7b73f3a3d39",
+    "url": "/monsters-rolodex/static/js/2.289458fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.289458fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5697b2cb5cf4dc56e81",
+    "url": "/monsters-rolodex/static/js/main.d2f72303.chunk.js"
+  },
+  {
+    "revision": "ea482232105d58993cc7",
+    "url": "/monsters-rolodex/static/js/runtime-main.497a71a2.js"
+  }
+]);
